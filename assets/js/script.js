115caa745b007fbe7a75fb2877b5960f987f6bee
kashifaxioned <kashifa@axioned.com>
2022-04-04 08:25:33 +0530

    styling added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,20 @@
 
 */
 
+const cards = document.querySelectorAll(".card");
 
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
+    removeExpand()
+    card.classList.add("expand")
+  })
+})
 
-
-
-
+function removeExpand() {
+  cards.forEach((card) => {
+    card.classList.remove("expand")
+  })
+}
 
 
 
